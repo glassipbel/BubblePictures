9e4d244d5ed2bdcc95732b20b375dbf3844c1202
kbelter <kevin.belter@gmail.com>
2018-09-24 09:28:12 -0500

    fix

--- Example/BubblePictures/AppDelegate.swift
+++ Example/BubblePictures/AppDelegate.swift
@@ -13,9 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
