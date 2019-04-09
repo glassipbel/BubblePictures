0ff7abd37d689c4f0210bfc9736b47b11dba66d8
kbelter <kevin.belter@gmail.com>
2019-04-09 10:21:13 -0500

    Updating to Swift 5.0

--- Example/BubblePictures/AppDelegate.swift
+++ Example/BubblePictures/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
 }
